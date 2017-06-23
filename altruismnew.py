cf0aa383de1868603759d37bce3e4b78d1ac4f8f
Richard Yang <ryang@redjava.ict.usc.edu>
2017-06-23 10:39:41 -0700

    New models

--- altruismnew.py
+++ altruismnew.py
@@ -16,58 +16,71 @@ world.defineState(banker.name,'pool',int)
 world.setState(banker.name,'pool',0)
 
 distribute = banker.addAction({'verb': 'distribute'})
+actors = []
 
 for i in range(4):
     actor = Agent('Agent'+str(i))
     world.addAgent(actor)
-    actor.setHorizon(15)
+    actor.setHorizon(5)
     world.defineState(actor.name,'money',float)
-    # Everyone starts with $200
-    # actor.setState('money',200)
-    '''
-    for i in range(20):
-        money = i*10
-        contrib = 'contribute '+str(money)
-        action = actor.addAction({'verb':contrib})
-        tree = makeTree(incrementMatrix(stateKey(None,'round'),1))
-        world.setDynamics(stateKey(None,'round'),action,tree)
 
-        tree = makeTree(incrementMatrix(stateKey(actor.name,'money'),money*-1))
-        world.setDynamics(stateKey(actor.name,'money'), action, tree)
+    # Start with one money unit
+    actor.setState('money',1.)
 
-        tree = makeTree(incrementMatrix(stateKey(banker.name,'pool'),money))
-        world.setDynamics(stateKey(banker.name, 'pool'), action, tree)
-    '''
-    actor.setState('money',5.)
+    # Don't give
     action = actor.addAction({'verb': 'pass'})
+    tree = makeTree(incrementMatrix(stateKey(None,'round'),1))
+    world.setDynamics(stateKey(None,'round'),action,tree)
+
+    # Donate
     action = actor.addAction({'verb': 'contribute'})
     tree = makeTree(incrementMatrix(stateKey(None,'round'),1))
     world.setDynamics(stateKey(None,'round'),action,tree)
 
+    # Remove money from agent
     tree = makeTree(incrementMatrix(stateKey(actor.name,'money'),-1))
     world.setDynamics(stateKey(actor.name,'money'), action, tree)
 
+    # Add money to pool
     tree = makeTree(incrementMatrix(stateKey(banker.name,'pool'),1))
     world.setDynamics(stateKey(banker.name, 'pool'), action, tree)
 
-    tree = makeTree(addFeatureMatrix(stateKey(actor.name,'money'),stateKey(banker.name, 'pool'),0.5))
+    # After each round, the banker distributes 90% of the pool value to every agent
+    tree = makeTree(addFeatureMatrix(stateKey(actor.name,'money'),stateKey(banker.name, 'pool'),0.9))
     world.setDynamics(stateKey(actor.name, 'money'),distribute,tree)
 
     actor.setReward(maximizeFeature(stateKey(actor.name, 'money')),1.0)
     #actor.setReward(maximizeFeature(stateKey(banker.name, 'pool')),1.0)
 
+    # TODO models
+    '''
     actor.addModel('Level0',R={},level=0,rationality=10.,selection='distribution')
     actor.addModel('Level1',R={},level=1,rationality=10.,selection='distribution')
     actor.addModel('Level2',R={},level=2,rationality=10.,selection='distribution')
 
-    '''
     for j in range(4):
         if not i == j:
             world.setMentalModel('Agent'+str(i),'Agent'+str(j),0.5)
         else:
             pass
     '''
+    actors.append(actor)
 
+# TODO Punish
+for i in range(4):
+    for j in range(4):
+        if not i == j:
+            me = actors[i]
+            other = actors[j]
+            action = me.addAction({'verb': 'punish', 'object': other.name})
+            tree = makeTree(setToConstantMatrix(stateKey(other.name,'money'),0))
+            world.setDynamics(stateKey(other.name,'money'),action,tree)
+        else:
+            pass
+
+
+
+# Max rounds
 world.addTermination(makeTree({'if': thresholdRow(stateKey(None,'round'),20),
                                 True: True,
                                 False: False}))
@@ -78,4 +91,11 @@ world.setOrder(['Agent0', 'Agent1', 'Agent2', 'Agent3', 'Banker'])
 while not world.terminated():
     result = world.step()
     world.explain(result, 2)
+
+    # print agent money
+    moneys = {}
+    for i in range(4):
+        me = actors[i]
+        moneys[i]=str(world.getState(me.name,'money').domain()[0])
+    print moneys
     print('Banker pool: ' + str(world.getState('Banker', 'pool').domain()[0]))
